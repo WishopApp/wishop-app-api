9c45233a3d1fde5634dedb0da6d23036da2b90e7
Chanon Pingpong <chanon.pingpong@gmail.com>
2018-04-19 13:58:12 +0700

    [ADD] new subCategoryProps path for fix issue path

---
+++ api/subCategoryProperty0/resolver.js
@@ -0,0 +1,29 @@
+import { baseResolver } from '../root/resolver'
+
+const subCategoryProp = baseResolver.createResolver(
+  async (root, args, context) => {
+    return context.subCategoryProp.getOne(args)
+  }
+)
+
+const subCategoryProps = baseResolver.createResolver(
+  async (root, args, context) => {
+    return context.subCategoryProp.getMany(args, args.limit, args.skip)
+  }
+)
+
+const createSubCategoryProp = baseResolver.createResolver(
+  async (root, args, context) => {
+    return context.subCategoryProp.create(args)
+  }
+)
+
+export default {
+  Query: {
+    subCategoryProp,
+    subCategoryProps
+  },
+  Mutation: {
+    createSubCategoryProp
+  }
+}
---
+++ api/subCategoryProperty0/schema.js
@@ -0,0 +1,8 @@
+export default [`
+type SubCategoryProp {
+  _id: ID
+  subCategoryId: [ID]
+  name: String
+  values: [String]
+}
+`]
