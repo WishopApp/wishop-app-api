6beaeb277b8b71af162d4360743cefb060d3004a
chanonpingpong <chanon.pingpong@gmail.com>
2018-09-26 12:44:15 +0700

    add sub-category for category

--- api/graphql/resolvers/category/query.js
+++ api/graphql/resolvers/category/query.js
@@ -8,9 +8,16 @@ const categories = baseResolver.createResolver(async (root, args, context) => {
   return context.models.category.getMany(args, args.limit, args.skip)
 })
 
+const subCategories = baseResolver.createResolver(async (category, args, context) => {
+  return context.models.subCategory.getMany({ categoryId: category._id })
+})
+
 module.exports = {
   Query: {
     category,
     categories,
+  },
+  Category: {
+    subCategories,
   },
 }
