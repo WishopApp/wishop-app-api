99387159ef97a0882ec23d55cd27c8da47d18112
chanonpingpong <chanon.pingpong@gmail.com>
2018-10-26 20:35:46 +0700

    update user status to shop owner when create store

--- api/graphql/resolvers/store/mutation.js
+++ api/graphql/resolvers/store/mutation.js
@@ -2,8 +2,15 @@ const { baseResolver } = require('../../../libaries/apollo-resolver-creator')
 
 const createStore = baseResolver.createResolver(async (root, args, context) => {
   const store = await context.models.store.create(args)
-  context.models.user.update({ _id: args.ownerId, storeId: store._id })
-  context.models.storeBranch.create({ storeId: store._id, name: 'Main' })
+  context.models.user.update({
+    _id: args.ownerId,
+    storeId: store._id,
+    status: 'SHOP_OWNER',
+  })
+  context.models.storeBranch.create({
+    storeId: store._id,
+    name: 'Main',
+  })
   return store
 })
 
